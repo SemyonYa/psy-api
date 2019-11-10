12aa0497188f1a78770423a2da95de4135ba5e7b
SemyonYa <mygarbage86@mail.ru>
2019-11-11 00:46:50 +0300

    + client

---
+++ models/Client.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "client".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ * @property integer $phone
+ * @property string $e-mail
+ */
+class Client extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'client';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'description', 'phone', 'e-mail'], 'required'],
+            [['description', 'e-mail'], 'string'],
+            [['phone'], 'integer'],
+            [['name'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'phone' => 'Phone',
+            'e-mail' => 'E Mail',
+        ];
+    }
+}
